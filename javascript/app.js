998b8b75e9f93dc25b943494753f2534254d88ed
Maria Kauffmann <mariatkauffmann@gmail.com>
2021-08-06 19:19:19 -0300

    nav fixed

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,31 @@
-let containerImg = document.querySelectorAll('.container-imagem')
-let btnProduto = document.querySelectorAll('.btn-produto')
+//fixed nav begin
+let fixedNav = document.querySelector('.nav-fixed');
+let body = document.body
+
+window.onscroll = function () {
+    if (body.scrollTop > 150 || document.documentElement.scrollTop > 150){
+        fixedNav.style.transform = "translate(0, 0)"
+    } else {
+        fixedNav.style.transform = "translate(0, -200px)"
+
+    }
+}
+
+//fixed nav end
+
+//botões de compra rápida begin
+let containerImg = document.querySelectorAll('.container-imagem');
+let btnProduto = document.querySelectorAll('.btn-produto');
 
 console.log(containerImg)
 
 for (let i = 0; i < containerImg.length; i++){
     containerImg[i].addEventListener('mouseover', function() {
-        btnProduto[i].style.transform = "translate(0, 0)"
+        btnProduto[i].style.transform = "translate(0, 0)";
     })
     
     containerImg[i].addEventListener('mouseout', function() {
-        btnProduto[i].style.transform = "translate(-300px, 0)"
+        btnProduto[i].style.transform = "translate(-300px, 0)";
     })
 }
+//botões de compra rápida end
